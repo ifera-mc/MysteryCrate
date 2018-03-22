a7504c9b7cf37dc20dc3f15d4c12884d17d804be
Jack M. Taylor <32965703+JackMD@users.noreply.github.com>
2018-03-22 16:41:46 +0500

    some missing stuff

--- src/JackMD/MysteryCrate/Task/RemoveChest.php
+++ src/JackMD/MysteryCrate/Task/RemoveChest.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ * MysteryCrate, a Crate plugin for PocketMine-MP
+ * Copyright (c) 2018 JackMD  < https://github.com/JackMD >
+ *
+ * This software is distributed under "GNU General Public License v3.0".
+ * You should have received a copy of the GNU General Public License v3.0
+ * along with this program.  If not, see
+ * <https://opensource.org/licenses/GPL-3.0>.
+ *
+ * ----------------------------------------------------------------------
+ */
+
 namespace JackMD\MysteryCrate\Task;
 
 use JackMD\MysteryCrate\Main;
