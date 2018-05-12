2fb173660e207a24719a474a54f3ef80a821236a
Jack M. Taylor <32965703+JackMD@users.noreply.github.com>
2018-05-12 15:45:14 +0500

    v5.0 updates

--- src/JackMD/MysteryCrate/Task/RemoveChest.php
+++ src/JackMD/MysteryCrate/Task/RemoveChest.php
@@ -1,15 +1,34 @@
 <?php
 
 /**
+ * ___  ___          _                  _____           _
+ * |  \/  |         | |                /  __ \         | |
+ * | .  . |_   _ ___| |_ ___ _ __ _   _| /  \/_ __ __ _| |_ ___
+ * | |\/| | | | / __| __/ _ \ '__| | | | |   | '__/ _` | __/ _ \
+ * | |  | | |_| \__ \ ||  __/ |  | |_| | \__/\ | | (_| | ||  __/
+ * \_|  |_/\__, |___/\__\___|_|   \__, |\____/_|  \__,_|\__\___|
+ *          __/ |                  __/ |
+ *         |___/                  |___/  By @JackMD for PMMP
+ *
  * MysteryCrate, a Crate plugin for PocketMine-MP
  * Copyright (c) 2018 JackMD  < https://github.com/JackMD >
+ * Discord: JackMD#3717
+ * Twitter: JackMTaylor_
  *
  * This software is distributed under "GNU General Public License v3.0".
- * You should have received a copy of the GNU General Public License v3.0
- * along with this program.  If not, see
- * <https://opensource.org/licenses/GPL-3.0>.
+ * This license allows you to use it and/or modify it but you are not at
+ * all allowed to sell this plugin at any cost. If found doing so the
+ * necessary action required would be taken.
  *
- * ----------------------------------------------------------------------
+ * MysteryCrate is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License v3.0 for more details.
+ *
+ * You should have received a copy of the GNU General Public License v3.0
+ * along with this program. If not, see
+ * <https://opensource.org/licenses/GPL-3.0>.
+ * ------------------------------------------------------------------------
  */
 
 namespace JackMD\MysteryCrate\Task;
@@ -21,29 +40,29 @@ use pocketmine\scheduler\PluginTask;
 
 class RemoveChest extends PluginTask
 {
-    private $plugin;
+    private $plugin, $cpos;
     public $chest;
 
-    /**
-     * RemoveChest constructor.
-     * @param Main $plugin
-     */
-    public function __construct(Main $plugin)
+	/**
+	 * RemoveChest constructor.
+	 *
+	 * @param Main    $plugin
+	 * @param Vector3 $cpos
+	 */
+	public function __construct(Main $plugin, Vector3 $cpos)
     {
         parent::__construct($plugin);
         $this->plugin = $plugin;
+        $this->cpos = $cpos;
     }
 
-    /**
-     * @param int $tick
-     */
-    public function onRun(int $tick)
+	/**
+	 * @param int $tick
+	 */
+	public function onRun(int $tick)
     {
-        $level = $this->plugin->getServer()->getLevelByName($this->plugin->crateWorld);
-        $cx = $this->plugin->X;
-        $cy = $this->plugin->Y;
-        $cz = $this->plugin->Z;
-        $cpos = new Vector3($cx, $cy, $cz);
+        $level = $this->plugin->getServer()->getLevelByName($this->plugin->getConfig()->get("crateWorld"));
+        $cpos = $this->cpos;
 
         $level->setBlock($cpos, Block::get(0));
 
