a936c5c21702c7a18b50d8567ab8dcf06563cb83
Jack M. Taylor <32965703+JackMD@users.noreply.github.com>
2018-05-25 15:54:02 +0500

    foramtting

--- src/JackMD/MysteryCrate/EventListener.php
+++ src/JackMD/MysteryCrate/EventListener.php
@@ -9,18 +9,23 @@
  * \_|  |_/\__, |___/\__\___|_|   \__, |\____/_|  \__,_|\__\___|
  *          __/ |                  __/ |
  *         |___/                  |___/  By @JackMD for PMMP
+ * 
  * MysteryCrate, a Crate plugin for PocketMine-MP
  * Copyright (c) 2018 JackMD  < https://github.com/JackMD >
+ * 
  * Discord: JackMD#3717
  * Twitter: JackMTaylor_
+ * 
  * This software is distributed under "GNU General Public License v3.0".
  * This license allows you to use it and/or modify it but you are not at
  * all allowed to sell this plugin at any cost. If found doing so the
  * necessary action required would be taken.
+ * 
  * MysteryCrate is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License v3.0 for more details.
+ * 
  * You should have received a copy of the GNU General Public License v3.0
  * along with this program. If not, see
  * <https://opensource.org/licenses/GPL-3.0>.
