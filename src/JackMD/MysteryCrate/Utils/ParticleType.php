497f818167c6814dce63c6d288fcfe133b018a53
JackMD <jackmtaylor.jmt@gmail.com>
2018-12-23 23:36:50 +0500

    Added ParticleType.php

---
+++ src/JackMD/MysteryCrate/Utils/ParticleType.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * ___  ___          _                  _____           _
+ * |  \/  |         | |                /  __ \         | |
+ * | .  . |_   _ ___| |_ ___ _ __ _   _| /  \/_ __ __ _| |_ ___
+ * | |\/| | | | / __| __/ _ \ '__| | | | |   | '__/ _` | __/ _ \
+ * | |  | | |_| \__ \ ||  __/ |  | |_| | \__/\ | | (_| | ||  __/
+ * \_|  |_/\__, |___/\__\___|_|   \__, |\____/_|  \__,_|\__\___|
+ *          __/ |                  __/ |
+ *         |___/                  |___/  By @JackMD for PMMP
+ *
+ * MysteryCrate, a Crate plugin for PocketMine-MP
+ * Copyright (c) 2018 JackMD  < https://github.com/JackMD >
+ *
+ * Discord: JackMD#3717
+ * Twitter: JackMTaylor_
+ *
+ * This software is distributed under "GNU General Public License v3.0".
+ * This license allows you to use it and/or modify it but you are not at
+ * all allowed to sell this plugin at any cost. If found doing so the
+ * necessary action required would be taken.
+ *
+ * MysteryCrate is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License v3.0 for more details.
+ *
+ * You should have received a copy of the GNU General Public License v3.0
+ * along with this program. If not, see
+ * <https://opensource.org/licenses/GPL-3.0>.
+ * ------------------------------------------------------------------------
+ */
+
+namespace JackMD\MysteryCrate\Utils;
+
+class ParticleType{
+
+	/** @var string */
+	public const CLOUD_RAIN = "CloudRain";
+    /** @var string */
+	public const CROWN = "Crown";
+	/** @var string */
+	public const DOUBLE_HELIX = "DoubleHelix";
+	/** @var string */
+	public const HELIX = "Helix";
+	/** @var string */
+	public const TING = "Ting";
+}
