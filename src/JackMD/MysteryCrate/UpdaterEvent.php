c94b561d98de22f075dcc41771e22b1fa60c49db
JackMD <jackmtaylor.jmt@gmail.com>
2018-12-23 22:57:19 +0500

    some reformatting of code

--- src/JackMD/MysteryCrate/UpdaterEvent.php
+++ src/JackMD/MysteryCrate/UpdaterEvent.php
@@ -34,11 +34,11 @@
 
 namespace JackMD\MysteryCrate;
 
-use JackMD\MysteryCrate\Utils\Lang;
-use JackMD\MysteryCrate\Task\PutChest;
-use JackMD\MysteryCrate\Task\RemoveChest;
 use DaPigGuy\PiggyCustomEnchants\CustomEnchants\CustomEnchants;
 use DaPigGuy\PiggyCustomEnchants\Main as CE;
+use JackMD\MysteryCrate\Task\PutChest;
+use JackMD\MysteryCrate\Task\RemoveChest;
+use JackMD\MysteryCrate\Utils\Lang;
 use pocketmine\block\Chest as ChestBlock;
 use pocketmine\command\ConsoleCommandSender;
 use pocketmine\item\enchantment\Enchantment;
@@ -52,13 +52,13 @@ use pocketmine\scheduler\Task;
 use pocketmine\tile\Chest as ChestTile;
 
 class UpdaterEvent extends Task{
-	
+
 	public $canTakeItem = false;
 	public $t_delay = 2 * 20;
 	public $main, $level, $plugin, $item, $player, $chest;
 	/** @var ChestBlock */
 	public $block;
-	
+
 	/**
 	 * UpdaterEvent constructor.
 	 *
@@ -67,122 +67,126 @@ class UpdaterEvent extends Task{
 	public function __construct(Main $plugin){
 		$this->plugin = $plugin;
 	}
-	
+
 	public function onRun(int $timer){
 		$t_delay = $this->getTDelay();
-		if($this->chest instanceof ChestTile != null){
+		$chestTile = $this->chest;
+		if(is_null($chestTile)){
+			return;
+		}
+		if($chestTile instanceof ChestTile){
 			$this->setTDelay(--$t_delay);
 			if($t_delay >= 0){
-				if($this->chest instanceof ChestTile){
-					$i = 0;
-					while($i < 27){
-						if($i != 4 && $i != 10 && $i != 11 && $i != 12 && $i != 13 && $i != 14 && $i != 15 && $i != 16 && $i != 22){
-							$this->setItemINT($i, 106, 1);
-						}
-						$i++;
+				$i = 0;
+				while($i < 27){
+					if($i != 4 && $i != 10 && $i != 11 && $i != 12 && $i != 13 && $i != 14 && $i != 15 && $i != 16 && $i != 22){
+						$this->setItemINT($i, 106, 1);
 					}
-					$this->setItemINT(4, 208, 1);
-					$this->setItemINT(22, 208, 1);
-					$block = $this->block;
-					$block->getLevel()->addSound(new ClickSound($block), [$this->player]);
-					$b = $block->getLevel()->getBlock($block->subtract(0, 1));
-					$type = $this->plugin->isCrateBlock($b->getId(), $b->getDamage());
-					$drops = array_rand($this->plugin->getCrateDrops($type), 1);
-					if(!is_array($drops)){
-						$drops = [$drops];
-					}
-					foreach($drops as $drop){
-						$values = $this->plugin->getCrateDrops($type)[$drop];;
-						$i = Item::get(($values["id"]), $values["meta"], $values["amount"]);
-						$i->setCustomName($values["name"]);
-						if(isset($values["enchantments"])){
-							foreach($values["enchantments"] as $enchantment => $enchantmentinfo){
-								$level = $enchantmentinfo["level"];
-								$ce = $this->plugin->getServer()->getPluginManager()->getPlugin("PiggyCustomEnchants");
-								if(!is_null($ce) && !is_null($enchant = CustomEnchants::getEnchantmentByName($enchantment))){
-									if($ce instanceof CE){
-										$i = $ce->addEnchantment($i, $enchantment, $level);
-									}
-								}else{
-									if(!is_null($enchant = Enchantment::getEnchantmentByName($enchantment))){
-										$i->addEnchantment(new EnchantmentInstance($enchant, $level));
-									}
+					$i++;
+				}
+				$this->setItemINT(4, 208, 1);
+				$this->setItemINT(22, 208, 1);
+				$block = $this->block;
+				$block->getLevel()->addSound(new ClickSound($block), [$this->player]);
+				$b = $block->getLevel()->getBlock($block->subtract(0, 1));
+				$type = $this->plugin->isCrateBlock($b->getId(), $b->getDamage());
+				$drops = array_rand($this->plugin->getCrateDrops($type), 1);
+				if(!is_array($drops)){
+					$drops = [$drops];
+				}
+				foreach($drops as $drop){
+					$values = $this->plugin->getCrateDrops($type)[$drop];;
+					$i = Item::get(($values["id"]), $values["meta"], $values["amount"]);
+					$i->setCustomName($values["name"]);
+					if(isset($values["enchantments"])){
+						foreach($values["enchantments"] as $enchantment => $enchantmentinfo){
+							$level = $enchantmentinfo["level"];
+							$ce = $this->plugin->getServer()->getPluginManager()->getPlugin("PiggyCustomEnchants");
+							if(!is_null($ce) && !is_null($enchant = CustomEnchants::getEnchantmentByName($enchantment))){
+								if($ce instanceof CE){
+									$i = $ce->addEnchantment($i, $enchantment, $level);
+								}
+							}else{
+								if(!is_null($enchant = Enchantment::getEnchantmentByName($enchantment))){
+									$i->addEnchantment(new EnchantmentInstance($enchant, $level));
 								}
 							}
 						}
-						if(isset($values["lore"])){
-							$i->setLore([$values["lore"]]);
-						}
-						if(isset($values["commands"])){
-							foreach($values["commands"] as $index => $cmd){
-								$nbt = $i->getNamedTag() ?? new CompoundTag("", []);
-								$cmd = str_replace(["%PLAYER%"], [$this->player->getName()], $cmd);
-								$nbt->setString($index, $cmd);
-								$i->setNamedTag($nbt);
-							}
-						}
-						$cInv = $this->chest->getInventory();
-						$this->setItem(10, $cInv->getItem(11), $cInv->getItem(11)->getCount(), $cInv->getItem(11)->getDamage());
-						$this->setItem(11, $cInv->getItem(12), $cInv->getItem(12)->getCount(), $cInv->getItem(12)->getDamage());
-						$this->setItem(12, $cInv->getItem(13), $cInv->getItem(13)->getCount(), $cInv->getItem(13)->getDamage());
-						$this->setItem(13, $cInv->getItem(14), $cInv->getItem(14)->getCount(), $cInv->getItem(14)->getDamage());//reward
-						$this->setItem(14, $cInv->getItem(15), $cInv->getItem(15)->getCount(), $cInv->getItem(15)->getDamage());
-						$this->setItem(15, $cInv->getItem(16), $cInv->getItem(16)->getCount(), $cInv->getItem(16)->getDamage());
-						$this->setItem(16, $i, $i->getCount(), $i->getDamage());
 					}
+					if(isset($values["lore"])){
+						$i->setLore([$values["lore"]]);
+					}
+					if(isset($values["commands"])){
+						foreach($values["commands"] as $index => $cmd){
+							$nbt = $i->getNamedTag() ?? new CompoundTag("", []);
+							$cmd = str_replace(["%PLAYER%"], [$this->player->getName()], $cmd);
+							$nbt->setString($index, $cmd);
+							$i->setNamedTag($nbt);
+						}
+					}
+					$cInv = $chestTile->getInventory();
+					$this->setItem(10, $cInv->getItem(11), $cInv->getItem(11)->getCount(), $cInv->getItem(11)->getDamage());
+					$this->setItem(11, $cInv->getItem(12), $cInv->getItem(12)->getCount(), $cInv->getItem(12)->getDamage());
+					$this->setItem(12, $cInv->getItem(13), $cInv->getItem(13)->getCount(), $cInv->getItem(13)->getDamage());
+					$this->setItem(13, $cInv->getItem(14), $cInv->getItem(14)->getCount(), $cInv->getItem(14)->getDamage());//reward
+					$this->setItem(14, $cInv->getItem(15), $cInv->getItem(15)->getCount(), $cInv->getItem(15)->getDamage());
+					$this->setItem(15, $cInv->getItem(16), $cInv->getItem(16)->getCount(), $cInv->getItem(16)->getDamage());
+					$this->setItem(16, $i, $i->getCount(), $i->getDamage());
 				}
 			}
 			if($t_delay == -1){
-				if($this->chest instanceof ChestTile){
-					$this->setItemINT(10, 0, 0);
-					$this->setItemINT(11, 0, 0);
-					$this->setItemINT(12, 0, 0);
-					$this->setItemINT(14, 0, 0);
-					$this->setItemINT(15, 0, 1);
-					$this->setItemINT(16, 0, 1);
-					$this->setCanTakeItem(true);
-					$slot13 = $this->chest->getInventory()->getItem(13);
-					$block = $this->block;
-					$b = $block->getLevel()->getBlock($block->subtract(0, 1));
-					$type = $this->plugin->isCrateBlock($b->getId(), $b->getDamage());
-					if($this->player instanceof Player){
-						if($slot13->getDamage() === $this->plugin->getConfig()->get("commandMeta")){
-							$nbt = $slot13->getNamedTag();
-							for($i = 0; $i < $this->plugin->getConfig()->get("maxCommands"); $i++){
-								if($nbt->hasTag($i, StringTag::class)){
-									$cmd = $nbt->getString($i);
-									$this->plugin->getServer()->dispatchCommand(new ConsoleCommandSender(), $cmd);
-								}
+				$this->setItemINT(10, 0, 0);
+				$this->setItemINT(11, 0, 0);
+				$this->setItemINT(12, 0, 0);
+				$this->setItemINT(14, 0, 0);
+				$this->setItemINT(15, 0, 1);
+				$this->setItemINT(16, 0, 1);
+				$this->setCanTakeItem(true);
+				$slot13 = $chestTile->getInventory()->getItem(13);
+				$block = $this->block;
+				$b = $block->getLevel()->getBlock($block->subtract(0, 1));
+				$type = $this->plugin->isCrateBlock($b->getId(), $b->getDamage());
+				if($this->player instanceof Player){
+					if($slot13->getDamage() === $this->plugin->getConfig()->get("commandMeta")){
+						$nbt = $slot13->getNamedTag();
+						for($i = 0; $i < $this->plugin->getConfig()->get("maxCommands"); $i++){
+							if($nbt->hasTag($i, StringTag::class)){
+								$cmd = $nbt->getString($i);
+								$this->plugin->getServer()->dispatchCommand(new ConsoleCommandSender(), $cmd);
 							}
-						}else{
-							$this->player->getInventory()->addItem($slot13);
-							$win_message = str_replace(["%REWARD%", "%COUNT%", "%CRATE%"], [$slot13->getName(), $slot13->getCount(), ucfirst($type)], Lang::$win_message);
-							$this->player->sendMessage($win_message);
 						}
+					}else{
+						$this->player->getInventory()->addItem($slot13);
+						$win_message = str_replace(["%REWARD%", "%COUNT%", "%CRATE%"], [
+							$slot13->getName(),
+							$slot13->getCount(),
+							ucfirst($type)
+						], Lang::$win_message);
+						$this->player->sendMessage($win_message);
 					}
-					$dmg = $block->getDamage();
-					$this->plugin->getScheduler()->scheduleDelayedTask(new RemoveChest($this->plugin, $block), 20);
-					$this->plugin->getScheduler()->scheduleDelayedTask(new PutChest($this->plugin, $block, $dmg), 24);
-					$this->plugin->getScheduler()->cancelTask($this->getTaskId());
 				}
+				$dmg = $block->getDamage();
+				$this->plugin->getScheduler()->scheduleDelayedTask(new RemoveChest($this->plugin, $block), 20);
+				$this->plugin->getScheduler()->scheduleDelayedTask(new PutChest($this->plugin, $block, $dmg), 24);
+				$this->plugin->getScheduler()->cancelTask($this->getTaskId());
 			}
 		}
 	}
-	
+
 	/**
 	 * @return int
 	 */
 	public function getTDelay(): int{
 		return $this->t_delay;
 	}
-	
+
 	/**
 	 * @param int $t_delay
 	 */
 	public function setTDelay(int $t_delay){
 		$this->t_delay = $t_delay;
 	}
-	
+
 	/**
 	 * @param     $index
 	 * @param int $id
@@ -197,7 +201,7 @@ class UpdaterEvent extends Task{
 			$this->chest->getInventory()->setItem($index, $item);
 		}
 	}
-	
+
 	/**
 	 * @param      $index
 	 * @param Item $item
@@ -211,14 +215,14 @@ class UpdaterEvent extends Task{
 			$this->chest->getInventory()->setItem($index, $item);
 		}
 	}
-	
+
 	/**
 	 * @return bool
 	 */
 	public function isCanTakeItem(): bool{
 		return $this->canTakeItem;
 	}
-	
+
 	/**
 	 * @param bool $canTakeItem
 	 */
